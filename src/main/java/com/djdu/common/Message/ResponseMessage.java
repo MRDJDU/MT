560a141693841df459611a752037f125cef8f442
MRDJDU <mrdjdu@qq.com>
2019-02-12 11:18:20 +0800

    封装的前端返回信息

---
+++ src/main/java/com/djdu/common/Message/ResponseMessage.java
@@ -0,0 +1,41 @@
+package com.djdu.common.Message;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName ResponseMessage
+ * @Description 返回对数据库操作的信息
+ * @Author DJDU
+ * @Date 2019/2/12 10:03
+ * @Version 1.0
+ **/
+@Component
+public class ResponseMessage<T> {
+    private int StatuCode;
+    private String Message;
+    private T data;
+
+    public int getStatuCode() {
+        return StatuCode;
+    }
+
+    public void setStatuCode(int statuCode) {
+        StatuCode = statuCode;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
