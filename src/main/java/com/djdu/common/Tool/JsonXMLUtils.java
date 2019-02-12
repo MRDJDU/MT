882165034768e6566419af54b74d59dbba197bce
MRDJDU <mrdjdu@qq.com>
2019-02-12 19:06:08 +0800

    定义了json转object工具，转换成需要的对象

---
+++ src/main/java/com/djdu/common/Tool/JsonXMLUtils.java
@@ -0,0 +1,31 @@
+package com.djdu.common.Tool;
+
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Map;
+
+/**
+ * @ClassName JsonXMLUtils
+ * @Description json转object工具，转换成需要的对象
+ * @Author DJDU
+ * @Date 2019/2/12 17:41
+ * @Version 1.0
+ **/
+public class JsonXMLUtils {
+    public static String obj2json(Object obj) throws Exception {
+        return JSON.toJSONString(obj);
+    }
+
+    public static <T> T json2obj(String jsonStr, Class<T> clazz) throws Exception {
+        return JSON.parseObject(jsonStr, clazz);
+    }
+
+    public static <T> Map<String, Object> json2map(String jsonStr)     throws Exception {
+        return JSON.parseObject(jsonStr, Map.class);
+    }
+
+    public static <T> T map2obj(Map<?, ?> map, Class<T> clazz) throws Exception {
+        return JSON.parseObject(JSON.toJSONString(map), clazz);
+    }
+}
