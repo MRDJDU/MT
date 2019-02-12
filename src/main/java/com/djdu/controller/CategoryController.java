ac98a26dd6cebf5ed664817b1eb797a189a8b4eb
MRDJDU <mrdjdu@qq.com>
2019-02-12 11:23:53 +0800

    controller层处理异常

--- src/main/java/com/djdu/controller/CategoryController.java
+++ src/main/java/com/djdu/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.djdu.controller;
 
 
+import com.djdu.common.Message.ResponseMessage;
 import com.djdu.dto.CategoryDto;
 import com.djdu.entity.Category;
 import com.djdu.service.CategoryService;
@@ -14,19 +15,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @ClassName CategoryDto
+ * @Description Category的Controller层，中间层，接收前端请求数据，给service处理
+ * @Author DJDU
+ * @Date 2019/1/27 20:42
+ * @Version 1.0
+ **/
 @RestController
 public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
+	@Autowired
+	ResponseMessage responseMessage;
 
+	/**
+	 * @Author DJDU
+	 * @Description 处理前端保存或更新分类的请求
+	 * @Date 2019/2/12 11:10
+	 * @Param [category]
+	 * @return com.djdu.common.Message.ResponseMessage
+	 **/
+	@PostMapping(name="/saveOrUpdateCategory",consumes= MediaType.APPLICATION_JSON_VALUE)
+	public  ResponseMessage saveOrUpdateCategory(@RequestBody Category category){
+		try{
+			categoryService.save(category);
+			responseMessage.setStatuCode(0);
+			responseMessage.setMessage("保存成功！");
+		}catch (Exception e){
+			responseMessage.setStatuCode(1);
+			responseMessage.setMessage("保存失败！");
+		}
+		return responseMessage;
+	}
+
+	/**
+	 * @Author DJDU
+	 * @Description 处理前端获取全部分类的请求
+	 * @Date 2019/2/12 11:09
+	 * @Param []
+	 * @return java.util.List<com.djdu.entity.Category>
+	 **/
 	@GetMapping("/findAllCategories")
-    public List<Category> list() throws Exception {
+    public List<Category> list(){
     	return categoryService.findAll();
     }
 
-	@PostMapping(name="/saveOrUpdateCategory",consumes= MediaType.APPLICATION_JSON_VALUE)
-	public  Category saveOrUpdateCategory(@RequestBody Category category) throws Exception {
-		return category;
-	}
 }
 
